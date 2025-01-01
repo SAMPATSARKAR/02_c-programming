7fc7f0e7a3ea7a94be9ea1037a3c8f2f659f727f
Sampat Sarkar <sampat7123@gmail.com>
2025-01-01 17:32:45 +0530

    commit 2

--- index.c
+++ index.c
@@ -1,9 +1,23 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    printf("Hello world\n");
-    printf("Answer %d\n", 42);
-    printf("Name %s\n", "Sarah");
-    printf("x %.1f i %d\n", 3.5, 100);
+// int main() {
+//     printf("Hello world\n");
+//     printf("Answer %d\n", 42);
+//     printf("Name %s\n", "Sarah");
+//     printf("x %.1f i %d\n", 3.5, 100);
+//     return 0;
+// }
+// #include <stdio.h>
+// int main(){
+//     printf("nn\n\nnn\n"); 
+//     printf("nn/n/nnn/n");
+//     return 0;
+// }
+#include <stdio.h>
+int main(){
+    printf("Hello World\n"); 
+    int x;
+    x=5;
+    printf("%d",x);
     return 0;
 }
